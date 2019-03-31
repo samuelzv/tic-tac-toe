9fffa838e4f30f90158d3546d60ea8b5e119a9d8
Samuel Zuniga Vanoye <samuelzv@gmail.com>
2019-03-30 18:12:47 -0600

    Add welcome component

---
+++ angular_app/lib/src/welcome/welcome_component.dart
@@ -0,0 +1,51 @@
+import 'dart:async';
+
+import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
+import 'package:angular_router/angular_router.dart';
+import 'package:angular_bloc/angular_bloc.dart';
+import 'package:angular_forms/angular_forms.dart';
+import '../route_paths.dart';
+
+@Component(
+  selector: 'welcome',
+  templateUrl: 'welcome_component.html' ,
+  styleUrls: [
+    'welcome_component.css'
+  ],
+  directives: [
+    materialInputDirectives,
+    formDirectives,
+    MaterialFabComponent,
+    MaterialButtonComponent,
+    MaterialIconComponent,
+    materialInputDirectives,
+    NgFor,
+    NgIf,
+    NgClass,
+  ],
+  pipes: [BlocPipe]
+)
+class WelcomeComponent implements OnInit, OnDestroy {
+  final Router _router;
+  final String welcomeMessage = 'Welcome!';
+  String userName;
+  ControlGroup welcomeForm;
+
+  WelcomeComponent(this._router);
+
+  @override
+  void ngOnInit() async {
+    welcomeForm = FormBuilder.controlGroup({
+      'username': ['', Validators.required]
+    });
+  }
+
+  @override
+  void ngOnDestroy() {
+  }
+
+  void onSubmit() {
+    this._router.navigate(RoutePaths.game.toUrl());
+  }
+}
