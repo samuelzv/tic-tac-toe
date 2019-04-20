8e67607fe5260a961b16eae283b2de02988e72eb
Samuel Zuniga Vanoye <samuelzv@gmail.com>
2019-04-20 16:03:53 -0500

    Extract function to generate the board

--- flutter_app/lib/pages/game/widgets/board.dart
+++ flutter_app/lib/pages/game/widgets/board.dart
@@ -4,6 +4,21 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:common/common.dart';
 
 class Board  extends StatelessWidget {
+  Widget _board(List<List<Cell>> cells) {
+    return GridView.count(
+              crossAxisCount: 3,
+              childAspectRatio: 1.0,
+              padding: EdgeInsets.all(3.0),
+              mainAxisSpacing: 3.0,
+              crossAxisSpacing: 3.0,
+              children: _getFlattenCells(cells)
+                .map((Cell cell) {
+                  return GridTile(
+                    child: Text('Hello'),
+                  );
+                }).toList()
+            );
+  }
   List<Cell> _getFlattenCells(List<List<Cell>> cells) {
     List<Cell> flatten = [];
 
@@ -22,20 +37,8 @@ class Board  extends StatelessWidget {
       bloc: BlocProvider.of<GameBloc>(context),
       builder: (BuildContext context, DataState gameState) {
         return Container(
-          height: 300.0,
-          child: GridView.count(
-              crossAxisCount: 3,
-              childAspectRatio: 1.0,
-              padding: EdgeInsets.all(3.0),
-              mainAxisSpacing: 3.0,
-              crossAxisSpacing: 3.0,
-              children: _getFlattenCells(gameState.cells)
-                .map((Cell cell) {
-                  return GridTile(
-                    child: Text('Hello'),
-                  );
-                }).toList()
-            )
+          height: 200.0,
+          child: _board(gameState.cells) 
         );
       }
     );
